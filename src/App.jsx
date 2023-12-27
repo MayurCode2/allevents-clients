1c06e005c0e889f4dc2c16a1c4c1a57582fc89d7
mayur patil <mayurpatil@mayurs-MacBook-Pro.local>
2023-12-27 11:18:37 +0530

    basic changes addevents

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
       
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add-events" element={<AddEvents />} />
+        <Route path="/addevent" element={<AddEvents />} />
       </Routes>
     </div>
   )
